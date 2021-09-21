2ae26f3d7381e4dc7cb8caf01e7f9e23fe3396db
hung1212 <929773789@qq.com>
2021-09-21 12:35:32 +0800

    学习 跨域问题二

--- src/api-study/http/api.js
+++ src/api-study/http/api.js
@@ -1,8 +1,9 @@
 const koa = require('koa')
 const app = new koa
+const proxy = require('koa-cache-proxy')
 
 const static = require('koa-static')
 
 app.use(static(__dirname+'/'))
-
+app.use(proxy({ match: '/api', host: 'http://localhost:4000' }));
 app.listen(3000)
--- src/api-study/http/index.js
+++ src/api-study/http/index.js
@@ -1,45 +1,54 @@
 const http = require('http')
 const fs = require('fs')
-http.createServer((request, response)=> {
+http.createServer((request, respose)=> {
     const { url, method, headers }= request
     console.log(url)
     console.log(method)
     if(url === '/' && method === 'GET') {
-        response.setHeader("Content-Type", 'text/html')
+        respose.setHeader("Content-Type", 'text/html')
         fs.readFile('./index.html', (err, data)=> {
             console.log(err)
             if(err) {
-                response.writeHead(500 , {
+                respose.writeHead(500 , {
                     "Content-Type": 'text/plain;charset=utf-8'
                 })
 
-                response.end('500 系统错误')
+                respose.end('500 系统错误')
             }
-            response.end(data)
+            respose.end(data)
         })
-    } else if(url === '/api/users' && method === 'GET') {
-        response.setHeader("Content-Type", 'application/json')
-        response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-        response.end(JSON.stringify({
+    } else if(url === '/api/users' && method === 'OPTIONS') {
+        respose.writeHead(200, {
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+            'Access-Control-Allow-Headers': 'X-token, Content-Type',
+            'Access-Control-Allow-Credentials': true
+        })
+        respose.end()
+    } else if((url === '/api/users') && ( method === 'GET' || method === 'POST' )) {
+        respose.setHeader("Content-Type", 'application/json')
+        respose.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        respose.setHeader('Set-Cookie', 'Cookiel=val123')
+        respose.setHeader('Access-control-Allow-Credentials', true)
+        respose.end(JSON.stringify({
             name: '李四'
         }))
     } else if(url === '/api/users?callback=callback' && method === 'GET') {
-        response.setHeader("Content-Type", 'application/json')
-        response.end(`callback({
+        respose.setHeader("Content-Type", 'application/json')
+        respose.end(`callback({
             name: '李四'
         })`)
     } else if(method === 'GET' && headers.accept.indexOf('image/*') > -1) {
         console.log('url',url)
         if(fs.existsSync('.'+url)) {
             let stream = fs.createReadStream('.' + url)
-            stream.pipe(response)
+            stream.pipe(respose)
         }
     } else {
-        response.writeHead( 404, {
+        respose.writeHead( 404, {
             "Content-Type": 'text/plain;charset=utf-8'
         })
-        response.end('404 页面不存在')
+        respose.end('404 页面不存在')
     }
 }).listen(4000, ()=> {
-    console.log('http://localhost:9090')
+    console.log('http://localhost:4000')
 })
